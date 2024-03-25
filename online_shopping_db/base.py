8a0f6bd607e88cea9b0fcdea31ae4ba43b8e606d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-03-25 17:37:32 +0100

    Add BaseTable class to online_shopping_db module

---
+++ online_shopping_db/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" This module defines the BaseTable class which inherits from the Base class. """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, DateTime, String
+from datetime import datetime
+
+Base = declarative_base()
+
+class BaseTable():
+        """ This class defines the BaseTable class which
+            inherits from the Base class.
+
+            Attributes:
+                created_at: A datetime object representing the time
+                the record was created.
+                updated_at: A datetime object representing
+                the time the record was updated.
+            Methods:
+                __repr__ : A method that returns a string representation
+                of the object.
+            """
+        __abstract__ = True
+
+        # created_at attribute using datetime.utcnow() function
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
+
+        def __repr__(self):
+                return f"{type(self).__name__:{self.__dict__}}"
